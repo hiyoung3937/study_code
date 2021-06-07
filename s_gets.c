8a47de9704475ea6dd817c6526511a46c26b2ce3
phil3937 <76479898+phil3937@users.noreply.github.com>
2021-06-07 16:32:18 +0800

    Add files via upload

---
+++ s_gets.c
@@ -0,0 +1,20 @@
+//代替gets函数，使其更加安全
+char *s_gets(char *st,int n);
+char *s_gets(char *st,int n)
+{
+    char * ret_val;
+    int i=0;
+    ret_val=fgets(st,n,stdin);
+    if(ret_val)//ret_val != NULL
+    {
+        whiel(st[i]!='\n' && st[i]!='\0')
+        i++;
+        if(st[i]=='\n')
+        st[i]='\0';
+        else 
+        while(getchar()!='\n')
+        continue;
+    }
+
+    return ret_val;
+}
